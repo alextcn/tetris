6ae6d04a3bd08ed1b6fb96d34aeedcef4d267acb
AntonErmolinsky <ermolinski94@mail.ru>
2015-12-14 17:32:40 +0300

    Added World module that manages game state and have function to change it.

---
+++ app/World.hs
@@ -0,0 +1,51 @@
+module World where
+
+import Config
+import Figures
+
+-- | Data represents the state of the Tetris game
+data TetrisGame = Game
+  { fallingFigure :: Figure
+  , nextFigure    :: Figure
+  , falledBlocks  :: [Block]
+  , isPause       :: Bool
+  } deriving (Show)
+
+
+-- | Initial state of the game
+initialState :: TetrisGame
+initialState 
+  = Game (toStart cube) (toStart cube) [] True
+
+-- | Translates a figure to Config.startPosition
+-- Should 
+toStart :: Figure -> Figure
+toStart (Figure bs)
+  = Figure (map (\(Block (x,y) sz) -> Block (x + sx, y + sy) sz) bs)
+    where
+      (sx, sy) = startPosition
+
+-- | Sets the currently falling figure from nextFigure
+nextFigureGame :: TetrisGame -> TetrisGame
+nextFigureGame (Game cur nxt bs pause) = Game nxt (toStart cube) bs pause
+
+-- | Shifts left a figure if able to
+shiftLeftGame :: TetrisGame -> TetrisGame
+shiftLeftGame game
+  | isPause game = game
+  | blockedFromLeft $ fallingFigure game = game
+  | otherwise = game { fallingFigure = shiftLeft $ fallingFigure game }
+
+-- | Shifts right a figure if able to
+shiftRightGame :: TetrisGame -> TetrisGame
+shiftRightGame game
+  | isPause game = game
+  | blockedFromRight $ fallingFigure game = game
+  | otherwise = game { fallingFigure = shiftRight $ fallingFigure game }
+
+-- | Shifts down a figure if able to
+shiftDownGame :: TetrisGame -> TetrisGame
+shiftDownGame game
+  | isPause game = game
+  | fell $ fallingFigure game = game
+  | otherwise = game { fallingFigure = shiftDown $ fallingFigure game }
